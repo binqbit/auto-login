0706b92ea8386042974a36187b024a4627b3215a
binqbit <128422584+binqbit@users.noreply.github.com>
2024-11-21 02:27:43 +0200

    improve tool

--- README.md
+++ README.md
@@ -1,19 +1,42 @@
-# auto-login - tool for automatic login in the terminal to any service
+# Auto Login
 
-## Install
-```shell
+### Description
+This is a simple tool that allows you to automatically log in to various services in the terminal.
+
+### Install
+```bash
 pip install -r requirements.txt
 ```
 
-## Example of usage
-- Auto login to the git
-- Auto login to the server
+### Parameters
+- `--admin`: This parameter is needed to provide access to the correct work if it is launched through the left software. `Optional`
+- `--cmd`: The command that will be executed. `Optional`
+- `--login`: Your login. `Optional`
+- `--password`: Your password or password name in the keyring. `Optional`
 
-## How to use
+### Keyring
+- Create `.env` file in the root directory.
+- Add the following lines to the file:
+    ```env
+    PASSWORDS = password_name1:password1, password_name2:password2
+    ```
+
+### Example Of Use
+- Auto git pull:
+    ```bash
+    auto-login --cmd "git pull" --login your_login --password github_password_name
+    ```
+
+- Auto login to the server:
+    ```bash
+    auto-login --cmd "ssh user@server" --login your_login --password your_password
+    ```
+
+### How To Use
 - Install the requirements.
 - Add this path to the system PATH variable.
 - Create a new shortcut on your desktop.
 - Open the shortcut properties.
-- Add the command to the target field: auto-login <--cmd command> <--admin --login your_login> <--password your_password>
+- Add the command to the target field: auto-login --admin --cmd <command> --login <your_login> --password <your_password>
 - Add keys shortcuts to run this file.
-- If you want to log into the service, focus on the terminal and press the keys shortcuts.
+- If you need to authenticate somewhere, you can press the key combination you need and the program will automatically enter the required credentials.
--- src/main.py
+++ src/main.py
@@ -1,53 +1,61 @@
 import os
 import time
-import pyperclip
 import pyautogui
+import dotenv
 import ctypes
 
-
-
 ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 6)
+dotenv.load_dotenv()
 
+PASSWORDS = {}
 
+passwords = os.getenv("PASSWORDS")
+if passwords:
+    passwords = passwords.split(",")
+    for password in passwords:
+        password = password.split(":")
+        if len(password) == 2:
+            PASSWORDS[password[0].strip()] = password[1].strip()
 
-args = ' '.join(os.sys.argv[1:])
+def get_param(name):
+    for i in range(len(os.sys.argv)):
+        if os.sys.argv[i] == name:
+            value = ""
+            for j in range(i + 1, len(os.sys.argv)):
+                if os.sys.argv[j].startswith("--"):
+                    break
+                value += os.sys.argv[j] + " "
+            value = value.strip()
+            if value:
+                return value
+    return None
 
-if "--password" in args:
-    [args, password] = args.split("--password")
-    password = password.strip()
-else:
-    password = None
+cmd = get_param("--cmd")
+cmd_wait = float(get_param("--cmd-wait") or "0.3")
 
-if "--login" in args:
-    [args, login] = args.split("--login")
-    login = login.strip()
-else:
-    login = None
+login = get_param("--login")
+login_wait = float(get_param("--login-wait") or "0.1")
 
-if "--cmd" in args:
-    [args, cmd] = args.split("--cmd")
-    cmd = cmd.strip()
-else:
-    cmd = None
+password = get_param("--password")
+password_wait = float(get_param("--password-wait") or "0.0")
 
+password_from_name = PASSWORDS.get(password)
+if password_from_name:
+    password = password_from_name
 
-
-time.sleep(0.3)
+time.sleep(0.1)
 
 if cmd:
-    time.sleep(0.1)
-    pyperclip.copy(cmd)
-    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.typewrite(cmd)
     pyautogui.press('enter')
+    time.sleep(cmd_wait)
 
 if login:
-    time.sleep(0.1)
-    pyperclip.copy(login)
-    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.typewrite(login)
     pyautogui.press('enter')
+    time.sleep(login_wait)
 
 if password:
-    time.sleep(0.1)
-    pyperclip.copy(password)
-    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.typewrite(password)
     pyautogui.press('enter')
+    time.sleep(password_wait)
